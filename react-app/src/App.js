6ae493ac5568414bc4115139884bb6af482f9abc
thao.vu <thao.vu2016@gmail.com>
2021-09-21 02:22:51 +0700

    added bootstrap to react

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,19 +1,46 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Product from './Product';
+import { Container, Row, Col } from 'react-bootstrap'
 
 function App() {
   let products = []
-  for(var i = 0; i < 10; ++i){
-    products.push({id: i, name: "Apple " + i, img: "https://picsum.photos/200"})
+  for (var i = 0; i < 12; ++i) {
+    products.push({ id: i, name: "Apple " + i, img: "https://picsum.photos/200", description: "description" })
   }
+  const col_num = 4
+  const createGrid = () => {
+    let grid = []
+    for (let i = 0; i < products.length; ++i) {
+      if (i % col_num === 0) {
+        grid.push([])
+      }
+      grid[parseInt(i / col_num)].push(products[i])
+    }
+    return grid
+  }
+  const rows = createGrid()
+
   return (
     <div className="App">
       <header className="App-header">
-        {
-          products.map(product => {
-            return <Product product={product}/>
-          })
-        }
+        <Container>
+          {
+            rows.map(cols => {
+              return (
+                <Row>
+                  {
+                    cols.map(col => {
+                      return (<Col>
+                        <Product product={col} />
+                      </Col>)
+                    })
+                  }
+                </Row>
+              )
+            })
+          }
+        </Container>
       </header>
     </div>
   );
--- react-app/src/Product.js
+++ react-app/src/Product.js
@@ -1,10 +1,34 @@
-import { Fragment } from "react";
-
-export default function Product(props){
-    return(
+import { Fragment, useState } from "react";
+import { Button, Card } from 'react-bootstrap'
+export default function Product(props) {
+    return (
         <Fragment>
-            <div>{props.product.name}</div>
-            <img src={props.product.img} alt={"Olleh"}/>
+            <Card>
+                <Card.Img src={props.product.img} />
+                <Card.Body>
+                    <Card.Title>{props.product.name}</Card.Title>
+                    <Card.Text>{props.product.description}</Card.Text>
+                </Card.Body>
+                <Button onClick={() => console.log('123')}>Add To Cart</Button>
+            </Card>
         </Fragment>
     )
+    // const [isToastDisplayed, showToast] = useState(false)
+    // const toggleToast = () => showToast(!isToastDisplayed)
+    // return (
+    //     <Fragment>
+    //         <Toast show={isToastDisplayed} onClose={toggleToast}>
+    //             <Toast.Header>Header</Toast.Header>
+    //             <Toast.Body>Body</Toast.Body>
+    //         </Toast>
+    //         <Card>
+    //             <Card.Img src={props.product.img}></Card.Img>
+    //             <Card.Body>
+    //                 <Card.Title>{props.product.name}</Card.Title>
+    //                 <Card.Text>{props.product.description}</Card.Text>
+    //             </Card.Body>
+    //         </Card>
+    //         <Button onClick={() => showToast(true)}>Show Toast</Button>
+    //     </Fragment>
+    // )
 }
